fd6714bc6f68701de7a8564eaa63cc3df5ce48ce
tanishq-tandon <146702442+tanishq-tandon@users.noreply.github.com>
2023-10-04 00:08:48 +0530

    Add files via upload

---
+++ Interest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    double p;
+    double t;
+    double r;
+
+    printf("Enter Principal Amount : ");
+    scanf("%lf",&p);
+
+    printf("Enter Rate of Interest : ");
+    scanf("%lf",&r);
+    
+    printf("Enter Time : ");
+    scanf("%lf",&t);
+   
+    
+}
