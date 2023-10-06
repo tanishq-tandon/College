15e5f5cd991585dc1fdc9c39ca0b87d6d0063df3
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-06 23:34:16 +0530

    Question 3

--- Swap.c
+++ Swap.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main()
+{
 
+    int a,b,c;
+    printf("Enter A : ");
+    scanf("%d",&a);
+
+    printf("Enter B : ");
+    scanf("%d",&b);
+    
+    c=a;    // Swapping
+    a=b;
+    b=c;
+
+    printf("\nA : %d",a);
+    printf("\nB : %d",b);
+    
+    return 0;
+}
