537d6952e95bf325faf5d07299ede51df76d0b68
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-07 23:23:54 +0530

    Question 5

--- Temperature.c
+++ Temperature.c
@@ -1,1 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    double c,f;
+    printf("Enter Temperature in Celsius : ");
+    scanf("%lf",&c);
+    f=((c/5)*9)+32;
 
+    printf("Temperature in Fahrenheit : %lf",f);
+
+    return 0;
+}
