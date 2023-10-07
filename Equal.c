5a6e9256b966006225443cba9e4691186185174e
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-07 23:34:03 +0530

    Question 6

--- Equal.c
+++ Equal.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    double a,b;
 
+    printf("Enter A : ");
+    scanf("%lf",&a);
+    printf("Enter B : ");
+    scanf("%lf",&b);
+
+    if(a==b)
+    {
+        printf("Both are Equal");
+    }
+    else
+    {
+        printf("Both are not Equal");
+    }
+    return 0;
+}
