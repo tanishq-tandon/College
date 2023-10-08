139e4b1c135e8c1526a47637ff9d32728f1f5aca
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-08 23:51:12 +0530

    Question 8

--- Even_Odd.c
+++ Even_Odd.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
 
+    if(n%2==0)
+    {
+        printf("Even Number ");
+    }
+    else
+    {
+        printf("Odd Number ");
+    }
+    return 0;
+}
