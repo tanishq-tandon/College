4c7986c80989a04d4e7408fed5d5ddf773ad068d
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-08 23:55:40 +0530

    Question 9

--- Leap_Year.c
+++ Leap_Year.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int y;
+    printf("Enter a Year : ");
+    scanf("%d",&y);
 
+    if(y%4==0 && y%400!=0)
+    {
+        printf("Leap Year");
+    }
+    else
+    {
+        printf("Not a Leap Year");
+    }
+}
