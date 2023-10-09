36332cea69e4107e73fe774ea4b758a793d9c8a6
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-09 23:52:30 +0530

    Question 10

--- Grades.c
+++ Grades.c
@@ -1,1 +1,28 @@
-
+#include<stdio.h>
+int main()
+{
+    int g;
+    printf("Enter Grades : ");
+    scanf("%d",&g);
+    if(g>90 && g<=100)
+    {
+        printf("Grade A");
+    }
+    else if(g>80 && g<=90)
+    {
+        printf("Grade B");
+    }
+    else if(g>60 && g<=80)
+    {
+        printf("Grade C");
+    }
+    else if(g>0 && g<=60)
+    {
+        printf("Grade D");
+    }
+    else
+    {
+        printf("Enter Valid Marks and try again.");
+    }
+    return 0;
+}
