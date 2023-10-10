6e1c9b11570920e5c96ec32c80a0a397715c6531
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-10 23:13:51 +0530

    Question 12

--- SumToN.c
+++ SumToN.c
@@ -11,4 +11,5 @@ int main()
     }
 
     printf("%d",sum);
+    return 0;
 }
--- Switch.c
+++ Switch.c
@@ -1,1 +1,45 @@
+#include<stdio.h>
+int main()
+{
+    double n1,n2;
+    char ch;
+    printf("Enter First Operand : ");
+    scanf("%lf",&n1);
+    printf("Enter the operator (+, -, *, /): ");
+    scanf("%c",&ch);
+    printf("Enter Second Operand : ");
+    scanf("%lf",&n1);
 
+    double result;
+
+    switch(ch)
+    {
+        case '+':
+            result = n1+n2;
+            break;
+        
+        case '-':
+            result = n1-n2;
+            break;
+
+        case '*':
+            result = n1*n2;
+            break;
+
+        case '/':
+            if(n2==0)
+            {
+                printf("Error : can not divide by zero");
+            }
+            else
+            {
+                result = n1/n2;
+            }
+            break;
+
+        default:
+            printf("Enter Valid Operator");
+    }
+    printf("Result : %lf",result);
+    return 0;
+}
