316925cb37413652781d529c4dc79450579edcc1
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-10 22:52:34 +0530

    Question 11

--- SumToN.c
+++ SumToN.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a Number : ");
+    scanf("%d",&n);
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
 
+    printf("%d",sum);
+}
