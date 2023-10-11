953d261cfada7bf52757fe8b7fb0c146727ac758
tanishq-tandon <tanishqtandon10a@gmail.com>
2023-10-11 23:10:56 +0530

    Question 11

--- Factorial.c
+++ Factorial.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,f=1;
+    printf("Enter a Number : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    printf("Factorial of %d is %d",n,f);
+    return 0;
+}
